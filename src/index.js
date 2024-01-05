55286f12ef36c097602af8326497c53e74261cbc
Tian Sheng <siewtiansheng@gmail.com>
2024-01-06 02:56:08 +0800

    add image & audio

--- src/index.js
+++ src/index.js
@@ -6,6 +6,19 @@ const noBtnContainer = document.getElementById("btn_no_container");
 const noBtnRect = noBtnContainer.getBoundingClientRect();
 const wrapper = document.getElementById("wrapper");
 const wrapperRect = wrapper.getBoundingClientRect();
+const sadSong = document.getElementById("sad_song");
+const happySong = document.getElementById("happy_song");
+const defaultSong = document.getElementById("default_song");
+const danceBean = document.getElementById("danceBean");
+const shockedBean = document.getElementById("shockedBean");
+const sadBean = document.getElementById("sadBean");
+const coffeeBean = document.getElementById("coffeeBean");
+const teaBean = document.getElementById("teaBean");
+const tastyBean = document.getElementById("tastyBean");
+const flirtBean = document.getElementById("flirtBean");
+const sadBean2 = document.getElementById("sadBean2");
+const cryBean = document.getElementById("cryBean");
+const laughBean = document.getElementById("laughBean");
 
 const getRandomNumber = (min, max) => {
   return Math.floor(Math.random() * (max - min)) + 1;
@@ -23,6 +36,8 @@ noBtnContainer.addEventListener("mouseover", () => {
     counter += 1;
   }
   if (counter === 15) {
+    cryBean.style.display = "none";
+    laughBean.style.display = "block";
     promptMsg.innerHTML = "give up, u can't say NO now HA!";
   }
   if (counter === 20) {
@@ -44,30 +59,43 @@ noBtn.addEventListener("click", () => {
     case 1:
       // are u sure?
       promptMsg.innerHTML = "jinjja??? 🤨";
+      shockedBean.style.display = "block";
       break;
     case 2:
       // really dw meh?
       promptMsg.innerHTML = "really dw meh? 😕";
+      shockedBean.style.display = "none";
+      sadBean.style.display = "block";
       break;
     case 3:
       // i buy u coffee
       promptMsg.innerHTML = "i buy u coffee ☕? 😏";
+      sadBean.style.display = "none";
+      coffeeBean.style.display = "block";
       break;
     case 4:
       // i buy u milk tea
       promptMsg.innerHTML = "i buy u milk tea 🧋? 😏";
+      coffeeBean.style.display = "none";
+      teaBean.style.display = "block";
       break;
     case 5:
       // i buy u ice cream
       promptMsg.innerHTML = "i buy u ice cream 🍦? 😋";
+      teaBean.style.display = "none";
+      tastyBean.style.display = "block";
       break;
     case 6:
       // i buy u coffee, milk tea & ice cream
       promptMsg.innerHTML = "i buy u coffee ☕, milk tea 🧋 & ice cream 🍦?";
+      tastyBean.style.display = "none";
+      flirtBean.style.display = "block";
       break;
     case 7:
       // click yes lah 🥺
       promptMsg.innerHTML = "go out with me laaaaa 🥺";
+      flirtBean.style.display = "none";
+      sadBean2.style.display = "block";
       break;
     case 8:
       // i very nice de 😢
@@ -80,6 +108,8 @@ noBtn.addEventListener("click", () => {
     case 10:
       // ❤️‍🩹
       promptMsg.innerHTML = "❤️‍🩹 ok u made me do this";
+      sadBean2.style.display = "none";
+      cryBean.style.display = "block";
       randomizePos();
       break;
     default:
@@ -95,4 +125,11 @@ yesBtn.addEventListener("click", () => {
   }
   noBtnContainer.style.display = "none";
   yesBtn.style.display = "none";
+  danceBean.style.display = "block";
+  happySong.play();
+  defaultSong.pause();
+});
+
+document.body.addEventListener("mousemove", () => {
+  defaultSong.play();
 });
